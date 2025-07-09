d9bdfe3bd7a84533c9647d9a3b21cdba6c158c27
Zeloux <164672397+Zeloux@users.noreply.github.com>
2025-07-09 14:40:29 +0200

    Improve entry orientation and crosshair detection

--- Horror Hospital/Assets/Scripts/MEchanichs/HideSpot.cs
+++ Horror Hospital/Assets/Scripts/MEchanichs/HideSpot.cs
@@ -16,17 +16,6 @@ public class HideSpot : MonoBehaviour
     public Transform[] exitPath;         // extra way-points on the way out
 
 #if UNITY_EDITOR
-
-    public virtual Quaternion GetEntryRotation()
-    {
-        if (lookAtWhenEntering)
-            return Quaternion.LookRotation(lookAtWhenEntering.forward, Vector3.up);
-        if (lookTarget)
-            return Quaternion.LookRotation(lookTarget.forward, Vector3.up);
-        return hidePoint ? hidePoint.rotation : transform.rotation;
-    }
-
-
     private void OnDrawGizmos()
     {
         // hide point ─ green
@@ -70,4 +59,15 @@ public class HideSpot : MonoBehaviour
         }
     }
 #endif
+
+    public virtual Quaternion GetEntryRotation()
+    {
+        if (entryPoints != null && entryPoints.Length > 0 && entryPoints[0])
+            return Quaternion.LookRotation(entryPoints[0].forward, Vector3.up);
+        if (lookAtWhenEntering)
+            return Quaternion.LookRotation(lookAtWhenEntering.forward, Vector3.up);
+        if (lookTarget)
+            return Quaternion.LookRotation(lookTarget.forward, Vector3.up);
+        return hidePoint ? hidePoint.rotation : transform.rotation;
+    }
 }
